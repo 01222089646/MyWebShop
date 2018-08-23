59c42a52e93ed2ceea79c411b23e56d101220854
01222089646 <tranngochieu1003@gmail.com>
2018-08-23 21:17:22 +0700

    aaa

--- src/Components/Help.js
+++ src/Components/Help.js
@@ -1,86 +1,64 @@
 import React from 'react';
-import {FormGroup,InputGroup,FormControl,Glyphicon,Button,DropdownButton,MenuItem} from 'react-bootstrap'
+import { FormGroup, InputGroup, FormControl,  Button, Row , Col  } from 'react-bootstrap'
 class Help extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        return (
-            <div>
-                <div>
-                <form>
-  <FormGroup>
-    <InputGroup>
-      <InputGroup.Addon>@</InputGroup.Addon>
-      <FormControl type="text" />
-    </InputGroup>
-  </FormGroup>
-  <FormGroup>
-    <InputGroup>
-      <FormControl type="text" />
-      <InputGroup.Addon>.00</InputGroup.Addon>
-    </InputGroup>
-  </FormGroup>
-  <FormGroup>
-    <InputGroup>
-      <InputGroup.Addon>$</InputGroup.Addon>
-      <FormControl type="text" />
-      <InputGroup.Addon>.00</InputGroup.Addon>
-    </InputGroup>
-  </FormGroup>
+  render() {
+    return (
+      <div>
+        <div>
+                  <h2>TRUNG TÂM TRỢ GIÚP KHÁCH HÀNG</h2>
 
-  <FormGroup>
-    <InputGroup>
-      <FormControl type="text" />
-      <InputGroup.Addon>
-        <Glyphicon glyph="music" />
-      </InputGroup.Addon>
-    </InputGroup>
-  </FormGroup>
+          <form>
+          <Col xs={6} md={4}>
+            <FormGroup>
+              <InputGroup>
+                <InputGroup.Addon >   What's your problem?   </InputGroup.Addon>
+                <FormControl type="text" />
+              </InputGroup>
+            </FormGroup>
+            <FormGroup>
+              <InputGroup>
+                <InputGroup.Addon>  Your   Name   </InputGroup.Addon>
+                <FormControl type="text" />
+              </InputGroup>
+            </FormGroup>
+            <FormGroup>
+              <InputGroup>
+                <InputGroup.Addon>    Phone number    </InputGroup.Addon>
+                <FormControl type="text" />
+              </InputGroup>
+            </FormGroup>
+            <FormGroup>
+              <InputGroup>
+                <InputGroup.Addon>    Address    </InputGroup.Addon>
+                <FormControl type="text" />
+              </InputGroup>
+            </FormGroup>
+            <Row>
+                    <Col xs={0} md={4} sm={3} lg={4}></Col>
+                    <Col xs={12} md={3} sm={6} lg={3}>
+                        <Button>Submit</Button>
+                    </Col>
+                                           
+                    <Col xs={12} md={4} sm={3} lg={4}></Col>
+            </Row>
+            </Col>
 
-  <FormGroup>
-    <InputGroup>
-      <InputGroup.Button>
-        <Button>Before</Button>
-      </InputGroup.Button>
-      <FormControl type="text" />
-    </InputGroup>
-  </FormGroup>
-  <FormGroup>
-    <InputGroup>
-      <FormControl type="text" />
-      <DropdownButton
-        componentClass={InputGroup.Button}
-        id="input-dropdown-addon"
-        title="Action"
-      >
-        <MenuItem key="1">Item</MenuItem>
-      </DropdownButton>
-    </InputGroup>
-  </FormGroup>
+            <h2></h2>
 
-  <FormGroup>
-    <InputGroup>
-      <InputGroup.Addon>
-        <input type="radio" aria-label="..." />
-      </InputGroup.Addon>
-      <FormControl type="text" />
-    </InputGroup>
-  </FormGroup>
-  <FormGroup>
-    <InputGroup>
-      <InputGroup.Addon>
-        <input type="checkbox" aria-label="..." />
-      </InputGroup.Addon>
-      <FormControl type="text" />
-    </InputGroup>
-  </FormGroup>
-</form>;
+
+
+   
+
+            
+          </form>;
                 </div>
-            </div>
-        )
-    }
+      </div>
+    )
+  }
 }
 
 export default Help;
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Row , Col , Thumbnail , Grid , Button ,Image} from 'react-bootstrap'
 
 class Home extends React.Component {
   constructor(props) {
@@ -8,7 +9,209 @@ class Home extends React.Component {
   render() {
     return (
       <div>
-        Home 
+            <Grid>
+  <Row>
+    <Col xs={6} md={4}>
+      <Thumbnail align="center">
+          <Image src="http://www.halfyourplate.ca/wp-content/uploads/2014/12/acerola_small.gif" responsive />
+        
+        <h3>Acerola</h3>
+        
+        <p>
+          <Button bsStyle="primary">Buy</Button>
+          &nbsp;
+          <Button bsStyle="default">Detail</Button>
+        </p>
+      </Thumbnail>
+    </Col>
+    <Col xs={6} md={4}>
+      <Thumbnail align="center" >
+      <Image src="http://www.halfyourplate.ca/wp-content/uploads/2014/12/acerola_small.gif" responsive />
+        <h3>Apple</h3>
+        
+        <p>
+          <Button bsStyle="primary">Buy</Button>
+          &nbsp;
+          <Button bsStyle="default">Detail</Button>
+        </p>
+      </Thumbnail>
+    </Col>
+    <Col xs={6} md={4}>
+      <Thumbnail align="center" >
+      <Image src="http://www.halfyourplate.ca/wp-content/uploads/2014/12/acerola_small.gif" responsive />
+        <h3>Apricot</h3>
+        
+        <p>
+          <Button bsStyle="primary">Buy</Button>
+          &nbsp;
+          <Button bsStyle="default">Detail</Button>
+        </p>
+      </Thumbnail>
+    </Col>
+  </Row>
+</Grid>;
+<Grid>
+  <Row>
+    <Col xs={6} md={4}>
+      <Thumbnail align="center" >
+      <Image src="http://www.halfyourplate.ca/wp-content/uploads/2014/12/acerola_small.gif" responsive />
+        <h3>Avocado</h3>
+        
+        <p>
+          <Button bsStyle="primary">Buy</Button>
+          &nbsp;
+          <Button bsStyle="default">Detail</Button>
+        </p>
+      </Thumbnail>
+    </Col>
+    <Col xs={6} md={4}>
+      <Thumbnail align="center" >
+      <Image src="http://www.halfyourplate.ca/wp-content/uploads/2014/12/acerola_small.gif" responsive />
+        <h3>Banana</h3>
+        
+        <p>
+          <Button bsStyle="primary">Buy</Button>
+          &nbsp;
+          <Button bsStyle="default">Detail</Button>
+        </p>
+      </Thumbnail>
+    </Col>
+    <Col xs={6} md={4}>
+      <Thumbnail align="center">
+      <Image src="http://www.halfyourplate.ca/wp-content/uploads/2014/12/acerola_small.gif" responsive />
+        <h3>Blackcurrant</h3>
+       
+        <p>
+          <Button bsStyle="primary">Buy</Button>
+          &nbsp;
+          <Button bsStyle="default">Detail</Button>
+        </p>
+      </Thumbnail>
+    </Col>
+  </Row>
+</Grid>;
+<Grid>
+  <Row>
+    <Col xs={6} md={4}>
+      <Thumbnail align="center" >
+      <Image src="http://www.halfyourplate.ca/wp-content/uploads/2014/12/acerola_small.gif" responsive />
+        <h3>Blueberries</h3>
+        <p>Description</p>
+        <p>
+          <Button bsStyle="primary">Buy</Button>
+          &nbsp;
+          <Button bsStyle="default">Detail</Button>
+        </p>
+      </Thumbnail>
+    </Col>
+    <Col xs={6} md={4}>
+      <Thumbnail align="center" >
+      <Image src="http://www.halfyourplate.ca/wp-content/uploads/2014/12/acerola_small.gif" responsive />
+        <h3>Breadfruit</h3>
+        
+        <p>
+          <Button bsStyle="primary">Buy</Button>
+          &nbsp;
+          <Button bsStyle="default">Detail</Button>
+        </p>
+      </Thumbnail >
+    </Col>
+    <Col xs={6} md={4}>
+      <Thumbnail  align="center">
+      <Image src="http://www.halfyourplate.ca/wp-content/uploads/2014/12/acerola_small.gif" responsive />
+        <h3>Cantaloupe</h3>
+        <p>
+          <Button bsStyle="primary">Buy</Button>
+          &nbsp;
+          <Button bsStyle="default">Detail</Button>
+        </p>
+      </Thumbnail>
+    </Col>
+  </Row>
+</Grid>;
+<Grid>
+  <Row>
+    <Col xs={6} md={4}>
+      <Thumbnail align="center">
+      <Image src="http://www.halfyourplate.ca/wp-content/uploads/2014/12/acerola_small.gif" responsive />
+        <h3>Carambola</h3>
+
+        <p>
+          <Button bsStyle="primary">Buy</Button>
+          &nbsp;
+          <Button bsStyle="default">Detail</Button>
+        </p>
+      </Thumbnail>
+    </Col>
+    <Col xs={6} md={4}>
+      <Thumbnail align="center" >
+      <Image src="http://www.halfyourplate.ca/wp-content/uploads/2014/12/acerola_small.gif" responsive />
+        <h3>Cherimoya</h3>
+       
+        <p>
+          <Button bsStyle="primary">Buy</Button>
+          &nbsp;
+          <Button bsStyle="default">Detail</Button>
+        </p>
+      </Thumbnail>
+    </Col>
+    <Col xs={6} md={4}>
+      <Thumbnail align="center" >
+      <Image src="http://www.halfyourplate.ca/wp-content/uploads/2014/12/acerola_small.gif" responsive />
+        <h3>Cherries</h3>
+        
+        <p>
+          <Button bsStyle="primary">Buy</Button>
+          &nbsp;
+          <Button bsStyle="default">Detail</Button>
+        </p>
+      </Thumbnail>
+    </Col>
+  </Row>
+</Grid>;
+<Grid>
+  <Row>
+    <Col xs={6} md={4}>
+      <Thumbnail align="center" >
+      <Image src="http://www.halfyourplate.ca/wp-content/uploads/2014/12/acerola_small.gif" responsive />
+        <h3>Clementine</h3>
+        
+        <p>
+          <Button bsStyle="primary">Buy</Button>
+          &nbsp;
+          <Button bsStyle="default">Detail</Button>
+        </p>
+      </Thumbnail>
+    </Col>
+    <Col xs={6} md={4}>
+      <Thumbnail align="center" >
+      <Image src="http://www.halfyourplate.ca/wp-content/uploads/2014/12/acerola_small.gif" responsive />
+        <h3>Coconutmeat</h3>
+        
+        <p>
+          <Button bsStyle="primary">Buy</Button>
+          &nbsp;
+          <Button bsStyle="default">Detail</Button>
+        </p>
+      </Thumbnail>
+    </Col>
+    <Col xs={6} md={4}>
+      <Thumbnail align="center">
+      <Image src="http://www.halfyourplate.ca/wp-content/uploads/2014/12/acerola_small.gif" responsive />
+        <h3>Cranberries</h3>
+        
+        <p>
+          <Button bsStyle="primary">Buy</Button>
+          &nbsp;
+          <Button bsStyle="default">Detail</Button>
+        </p>
+      </Thumbnail>
+    </Col>
+  </Row>
+</Grid>;
+
+
+
       </div>
     );
   }
